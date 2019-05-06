4319d528b98dec93b2af76b2f57ed44572dd8c11
Taleb Said <saidtaleb128@gmail.com>
2019-05-06 21:30:34 +0000

    factory method

---
+++ factoryMethod/Cooling.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FactoryMethod
+{
+    public class Cooling : IAirConditioner
+    {
+        private readonly double _temperature;
+
+        public Cooling(double temperature)
+        {
+            _temperature = temperature;
+        }
+
+        public void Operate()
+        {
+            Console.WriteLine($"Cooling the room to the required temperature of {_temperature} degrees");
+        }
+    }
+}
---
+++ factoryMethod/WarmingFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FactoryMethod
+{
+    public class WarmingFactory : AirConditionerFactory
+    {
+        public override IAirConditioner Create(double temperature) => new Warming(temperature);
+    }
+}
